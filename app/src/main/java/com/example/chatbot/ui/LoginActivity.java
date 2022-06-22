1993af688aee663bac24985a95558bbe8a9a621f
Terry <terrybmochire@gmail.com>
2022-06-22 22:05:18 +0300

    Navigate to sign up from log in activity.

--- app/src/main/java/com/example/chatbot/ui/LoginActivity.java
+++ app/src/main/java/com/example/chatbot/ui/LoginActivity.java
@@ -1,16 +1,36 @@
 package com.example.chatbot.ui;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.chatbot.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.createnewac) TextView mCreateNewAcc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mCreateNewAcc.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if( v == mCreateNewAcc) {
+            startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+
+        }
     }
 }
