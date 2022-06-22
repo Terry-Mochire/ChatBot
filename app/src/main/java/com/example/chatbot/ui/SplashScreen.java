513fd1dd8b45bc7b48cec4a60505a6b6855f1897
Terry <terrybmochire@gmail.com>
2022-06-22 21:51:12 +0300

    Implement splash screen .

--- app/src/main/java/com/example/chatbot/ui/SplashScreen.java
+++ app/src/main/java/com/example/chatbot/ui/SplashScreen.java
@@ -2,7 +2,9 @@ package com.example.chatbot.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.chatbot.R;
 
@@ -12,5 +14,12 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, LoginActivity.class));
+                finish();
+            }
+        }, 2000);
     }
 }
