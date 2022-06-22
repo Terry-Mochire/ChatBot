dfd7d0c853eed1cbcd978be79b5363e661e9009f
Terry <terrybmochire@gmail.com>
2022-06-22 22:08:47 +0300

    Implement navigation to sign in from sign up activity.

--- app/src/main/java/com/example/chatbot/ui/SignUpActivity.java
+++ app/src/main/java/com/example/chatbot/ui/SignUpActivity.java
@@ -1,15 +1,34 @@
 package com.example.chatbot.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.chatbot.R;
 
-public class SignUpActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.signinInstead) TextView  msignInInstead;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        ButterKnife.bind(this);
+        msignInInstead.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == msignInInstead) {
+            startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+        }
     }
 }
