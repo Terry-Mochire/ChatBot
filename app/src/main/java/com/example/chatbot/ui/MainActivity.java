5eb2c67873fefd0876c2240dede06201c7acc03e
Terry <terrybmochire@gmail.com>
2022-06-23 04:28:12 +0300

    Create overflow menu on Main activity.

--- app/src/main/java/com/example/chatbot/ui/MainActivity.java
+++ app/src/main/java/com/example/chatbot/ui/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.chatbot.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.example.chatbot.R;
 
@@ -12,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
