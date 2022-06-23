89193330008610e4aca610bcfdd0dd84b67c4167
Terry <terrybmochire@gmail.com>
2022-06-23 08:48:28 +0300

    Implement the API.

--- app/src/main/java/com/example/chatbot/ui/MainActivity.java
+++ app/src/main/java/com/example/chatbot/ui/MainActivity.java
@@ -1,22 +1,119 @@
 package com.example.chatbot.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.chatbot.R;
+import com.example.chatbot.adapters.MessageRVAdapter;
+import com.example.chatbot.models.Chats;
+import com.example.chatbot.models.Message;
+import com.example.chatbot.network.Api;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.idRVChats) RecyclerView chatsRV;
+    @BindView(R.id.idIBSend) FloatingActionButton sendMsgFab;
+    @BindView(R.id.idEdtMessage) EditText userEditMessage;
+
+    private final String USER_KEY = "user";
+    private final String BOT_KEY = "bot";
+    private ArrayList<Chats> messageModalArrayList;
+    private MessageRVAdapter messageRVAdapter;
+    static String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        messageModalArrayList = new ArrayList<>();
+
+
+        sendMsgFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (userEditMessage.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter your message..", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                sendMessage(userEditMessage.getText().toString());
+                userEditMessage.setText("");
+            }
+        });
+
+        messageRVAdapter = new MessageRVAdapter(messageModalArrayList, this);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this, RecyclerView.VERTICAL, false);
+
+        chatsRV.setLayoutManager(linearLayoutManager);
+        chatsRV.setAdapter(messageRVAdapter);
+    }
+
+    private void sendMessage(String message) {
+        messageModalArrayList.add(new Chats(message, USER_KEY));
+        messageRVAdapter.notifyDataSetChanged();
+
+        String url = "http://api.brainshop.ai/get?bid=167219&key=xbLKMgn3DuD9X6U5&uid=[uid]&msg=" +message;
+        String BASE_URL = "http://api.brainshop.ai/";
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        Api api = retrofit.create(Api.class);
+        Call<Message> call = api.getMessage(url);
+        call.enqueue(new Callback<Message>() {
+            @Override
+            public void onResponse(Call<Message> call, Response<Message> response) {
+                if (response.isSuccessful()) {
+                    Message message1 = response.body();
+                    messageModalArrayList.add(new Chats(message1.getCnt(), BOT_KEY));
+                    messageRVAdapter.notifyDataSetChanged();
+                } else {
+                    Log.e(TAG, "Response not successful");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Message> call, Throwable t) {
+                messageModalArrayList.add(new Chats("Please try another question", BOT_KEY));
+                messageRVAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
     @Override
